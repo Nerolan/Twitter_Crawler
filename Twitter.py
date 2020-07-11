7a7ea1624692fcf0809dcb1ac07591d0398c10c0
Pa1rick <p.suehrer@gmail.com>
2020-07-11 17:28:17 +0200

    update

--- Twitter.py
+++ Twitter.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import tweepy
 from textblob import TextBlob
-import preprocessor as p
 import nltk
 import re
 import string
 from csv import writer
+import time
+from datetime import datetime
 
+print("Process started")
 
 
 credentials = pd.read_csv("credentials.csv", sep=";")
@@ -50,26 +52,36 @@ def get_influencer_tweets(name, count):
     df['Polarity'] = df['Tweets'].apply(get_polarity)
     return df
 
-def get_tweets_by_hashtags(hashtag, date="2020-01-01",nums=20):
+def get_tweets_by_hashtags(hashtag, date="2020-01-01",nums=200):
     hashtag += " -filter:retweets"
     tweets = tweepy.Cursor(api.search, q=hashtag, lang="en", since=date,tweet_mode="extended").items(nums)
     hashtag_info = [[tweet.full_text,tweet.user.screen_name, tweet.user.location, tweet.user.followers_count,
                     tweet.user.friends_count,tweet.user.statuses_count,tweet.user.created_at,tweet.user.description,
-                    tweet.user.verified, tweet.user.favourites_count]
+                    tweet.user.verified, tweet.user.favourites_count, pd.to_datetime("now")]
                     for tweet in tweets]
 
     df_hashtag = pd.DataFrame(data=hashtag_info,columns=['Tweets','User','Location','Follower Count','Following Count',
                                                         'Number of Tweets','Account Created Date','Account Description',
-                                                        'Verified','Favourites Count'])
+                                                        'Verified','Favourites Count','Date'])
     df_hashtag['Tweets'] = df_hashtag['Tweets'].apply(clean_txt)
     df_hashtag['Account Description'] = df_hashtag['Account Description'].apply(clean_txt)
     df_hashtag['Subjectivity'] = df_hashtag['Tweets'].apply(get_subjectivity)
     df_hashtag['Polarity'] = df_hashtag['Tweets'].apply(get_polarity)
+    print("Added to existing CSV:  "+str(len(df_hashtag)))
     return df_hashtag
-    append_list_as_row("hashtag_tweets.csv",df_hashtag)
+#    append_list_as_row("blacklivesmatter.csv",df_hashtag)
 
 
 #get_influencer_tweets("@realDonaldTrump", 50).to_csv('Trump_Tweets.csv')
 
 #if file already exists in folder --> header=False
-get_tweets_by_hashtags("#blacklivesmatter").to_csv("hashtag_tweets.csv", mode='a', header=False, index=False)
+def run():
+    print("\n \n __Triggered again__")
+    localtime = time.localtime()
+    result = time.strftime("%I:%M:%S %p", localtime)
+    print(result)
+    time.sleep(70)
+    get_tweets_by_hashtags("#blacklivesmatter").to_csv("blacklivesmatter.csv", mode='a', header=False, index=False)
+    run()
+
+run()
